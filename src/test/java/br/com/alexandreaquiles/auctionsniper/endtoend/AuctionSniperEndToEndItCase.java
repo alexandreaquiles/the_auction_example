7f09ebe372667f114bac97454d54782ab384ef76
Alexandre <alexandre.aquiles@gmail.com>
2012-08-17 19:03:55 -0300

    Implementing FakeAuctionServer

--- src/test/java/br/com/alexandreaquiles/auctionsniper/endtoend/AuctionSniperEndToEndItCase.java
+++ src/test/java/br/com/alexandreaquiles/auctionsniper/endtoend/AuctionSniperEndToEndItCase.java
@@ -7,7 +7,7 @@ public class AuctionSniperEndToEndItCase {
 	private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
 	private final ApplicationRunner application = new ApplicationRunner();
 	
-	@Test public void sniperJoinAuctionUntilAuctionCloses() {
+	@Test public void sniperJoinAuctionUntilAuctionCloses() throws Exception {
 		auction.startSellingItem();
 		application.startBiddingIn(auction);
 		auction.hasReceivedJoinRequestFromSniper();
--- src/test/java/br/com/alexandreaquiles/auctionsniper/endtoend/FakeAuctionServer.java
+++ src/test/java/br/com/alexandreaquiles/auctionsniper/endtoend/FakeAuctionServer.java
@@ -1,27 +1,82 @@
 package br.com.alexandreaquiles.auctionsniper.endtoend;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManagerListener;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+
 public class FakeAuctionServer {
 	
-	protected static final String XMPP_HOSTNAME = null;
+	
+	public static final String XMPP_HOSTNAME = "localhost";
+
+	private static final String ITEM_ID_AS_LOGIN = "auction-%s";
+	private static final String AUCTION_PASSWORD = "auction";
+	private static final String AUCTION_RESOURCE = "Auction";
+
+	private final SingleMessageListener messageListener = new SingleMessageListener();
+	
+	private String itemId;
+	private XMPPConnection connection;
+	private Chat currentChat;
 
 
-	public FakeAuctionServer(String auction) {
+	public FakeAuctionServer(String itemId) {
+		this.itemId = itemId;
+		this.connection = new XMPPConnection(XMPP_HOSTNAME);
 	}
 
-	public void startSellingItem() {
+	public void startSellingItem() throws XMPPException {
+		connection.connect();
+		String username = String.format(ITEM_ID_AS_LOGIN, itemId);
+		connection.login(username, AUCTION_PASSWORD, AUCTION_RESOURCE);
+		connection.getChatManager().addChatListener(
+				new ChatManagerListener() {
+					public void chatCreated(Chat chat, boolean createdLocally) {
+						currentChat = chat;
+						chat.addMessageListener(messageListener);
+					}
+				}
+		);
 	}
 
-	public void hasReceivedJoinRequestFromSniper() {
+	public void hasReceivedJoinRequestFromSniper() throws InterruptedException {
+		messageListener.receivesAMessage();
 	}
 
-	public void announceClosed() {
+	public void announceClosed() throws XMPPException {
+		currentChat.sendMessage(new Message());
 	}
 
 	public void stop() {
+		connection.disconnect();
 	}
 
 	public String getItemId() {
-		return null;
+		return itemId;
 	}
 
+}
+
+class SingleMessageListener implements MessageListener {
+	
+	private final ArrayBlockingQueue<Message> messages = new ArrayBlockingQueue<Message>(1);
+
+	public void processMessage(Chat chat, Message message) {
+		messages.add(message);
+	}
+
+	public void receivesAMessage() throws InterruptedException {
+		assertThat("Message", messages.poll(5, TimeUnit.SECONDS), is(notNullValue()));
+	}
+	
 }
