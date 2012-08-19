0003a285e9a8021192400bad9779c9aa8335259e
Alexandre <alexandre.aquiles@gmail.com>
2012-08-19 12:56:43 -0300

    Extracting XMPPAuction class from Main and extracting JOIN message to XMPPAuction

--- src/main/java/br/com/alexandreaquiles/auctionsniper/Auction.java
+++ src/main/java/br/com/alexandreaquiles/auctionsniper/Auction.java
@@ -2,4 +2,5 @@ package br.com.alexandreaquiles.auctionsniper;
 
 public interface Auction {
 	void bid(int amount);
+	void join();
 }
--- src/main/java/br/com/alexandreaquiles/auctionsniper/Main.java
+++ src/main/java/br/com/alexandreaquiles/auctionsniper/Main.java
@@ -13,6 +13,30 @@ import br.com.alexandreaquiles.auctionsniper.ui.MainWindow;
 
 public class Main implements SniperListener {
 	
+	private final class XMPPAuction implements Auction {
+		private final Chat chat;
+
+		private XMPPAuction(Chat chat) {
+			this.chat = chat;
+		}
+
+		public void bid(int amount) {
+			sendMessage(String.format(BID_COMMAND_FORMAT, amount));
+		}
+
+		public void join() {
+			sendMessage(JOIN_COMMAND_FORMAT);
+		}
+
+		private void sendMessage(String message) {
+			try {
+				chat.sendMessage(message);
+			} catch (XMPPException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	private static final int ARG_HOSTNAME = 0;
 	private static final int ARG_USERNAME = 1;
 	private static final int ARG_PASSWORD = 2;
@@ -55,19 +79,11 @@ public class Main implements SniperListener {
 		final Chat chat = connection.getChatManager().createChat(auctionId(connection, itemId), null);
 		notToBeGCD = chat;
 
-		Auction auction = new Auction(){
-			public void bid(int amount) {
-				try {
-					chat.sendMessage(String.format(BID_COMMAND_FORMAT, amount));
-				} catch (XMPPException e) {
-					e.printStackTrace();
-				}
-			}
-		};
+		Auction auction = new XMPPAuction(chat);
 
 		chat.addMessageListener(new AuctionMessageTranslator(new AuctionSniper(auction, this)));
 		
-		chat.sendMessage(JOIN_COMMAND_FORMAT);
+		auction.join();
 	}
 
 	private void disconnectWhenUICloses(final XMPPConnection connection) {
